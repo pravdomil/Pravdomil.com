3e20f01f973dfbe588d650415a6186d0545e2afd
pravdomil <pravdomil.toman@gmail.com>
2021-07-26 16:29:49 +0200

    Translation

--- src/Translation.elm
+++ src/Translation.elm
@@ -1,16 +1,5 @@
 module Translation exposing (..)
 
 
-type Translation
-    = A_Title
-    | A_Raw String
-
-
-t : Translation -> String
-t a =
-    case a of
-        A_Title ->
-            "Pravdomil.com"
-
-        A_Raw b ->
-            b
+title =
+    "Pravdomil.com"
