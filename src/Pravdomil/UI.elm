93abd8ac87ecb818bf925a716ed7f0cbc1365051
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-02-01 00:05:58 +0100

    Pravdomil.UI

--- src/Pravdomil/UI.elm
+++ src/Pravdomil/UI.elm
@@ -1,7 +1,29 @@
 module Pravdomil.UI exposing (..)
 
-import Element.PravdomilUI.LightTheme
+import Element.PravdomilUI exposing (..)
+import Element.PravdomilUI.Theme.Light
 
 
 theme =
-    Element.PravdomilUI.LightTheme.theme
+    Element.PravdomilUI.Theme.Light.theme style
+        |> (\v ->
+                { v
+                    | page =
+                        v.page
+                            ++ [ bgColor Element.PravdomilUI.Theme.Light.style.black0
+                               ]
+                    , heading3 =
+                        v.heading3
+                            ++ [ fontSize 20
+                               ]
+                }
+           )
+
+
+style =
+    Element.PravdomilUI.Theme.Light.style
+        |> (\v ->
+                { v
+                    | primary = rgb 0 0 0.8
+                }
+           )
