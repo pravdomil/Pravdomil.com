b448bad75a730960b513ecafc8f24221594d2ada
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-10-25 11:24:20 +0200

    next

--- src-external/GitHub/Token.elm
+++ src-external/GitHub/Token.elm
@@ -3,3 +3,13 @@ module GitHub.Token exposing (..)
 
 type Token
     = Token String
+
+
+fromString : String -> Token
+fromString =
+    Token
+
+
+toString : Token -> String
+toString (Token a) =
+    a
