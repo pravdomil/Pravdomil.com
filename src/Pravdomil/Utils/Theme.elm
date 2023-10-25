8d82a2f76526384ba473ff53c7769fb116112513
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-10-25 11:36:43 +0200

    next

--- src/Pravdomil/Utils/Theme.elm
+++ src/Pravdomil/Utils/Theme.elm
@@ -1,25 +1,5 @@
 module Pravdomil.Utils.Theme exposing (..)
 
-import Element.PravdomilUi exposing (..)
-import Element.PravdomilUi.Theme.Basic
-
-
-theme =
-    Element.PravdomilUi.Theme.Basic.theme style
-        |> (\x ->
-                { x
-                    | page =
-                        \x2 -> x.page (bgColor Element.PravdomilUi.Theme.Basic.style.back100 :: x2)
-                    , heading3 =
-                        \x2 -> x.heading3 (fontSize 20 :: x2)
-                }
-           )
-
-
-style =
-    Element.PravdomilUi.Theme.Basic.style
-        |> (\x ->
-                { x
-                    | primaryBack = rgb 0 0 0.8
-                }
-           )
+import Element exposing (..)
+import Element.Font
+import Element.Region
