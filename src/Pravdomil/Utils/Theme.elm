2ba096328d25416fd91fdbaddf1dfea4dc5de58e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-10-25 11:37:06 +0200

    next

--- src/Pravdomil/Utils/Theme.elm
+++ src/Pravdomil/Utils/Theme.elm
@@ -3,3 +3,27 @@ module Pravdomil.Utils.Theme exposing (..)
 import Element exposing (..)
 import Element.Font
 import Element.Region
+
+
+blue =
+    rgb 0 0 0.8
+
+
+
+--
+
+
+page a =
+    a
+
+
+heading1 a =
+    Element.Region.heading 1 :: a
+
+
+heading2 a =
+    Element.Region.heading 2 :: a
+
+
+heading3 a =
+    Element.Region.heading 3 :: Element.Font.size 20 :: a
